9343cc1120cab6a94563cc0c12c17ad886913313
João Santos <jfasantos@gmail.com>
2022-04-13 13:00:08 +0100

    Adding code for api development

--- main.py
+++ main.py
@@ -1,1 +1,102 @@
 # Put the code for your API here.
+
+
+import logging
+import pickle
+import os
+from typing import List
+import pandas as pd
+# BaseModel from Pydantic is used to define data objects.
+from pydantic import BaseModel, Field
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+
+from starter.ml.data import process_data
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+# load our model artifacts
+root_path = os.path.join(os.getcwd())
+logger.info("Uploading model artifacts")
+filename = '/gbm_model.pickle'
+model = pickle.load(open(os.path.join(root_path, 'model') + filename, 'rb'))
+filename = '/model_encoder.pickle'
+encoder = pickle.load(open(os.path.join(root_path, 'model') + filename, 'rb'))
+filename = '/label_encoder.pickle'
+lb = pickle.load(open(os.path.join(root_path, 'model') + filename, 'rb'))
+
+
+# Declare the data object with its components and their type.
+class InputSchema(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int = Field(alias="education-num")
+    marital_status: str = Field(alias="marital-status")
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: float = Field(alias="capital-gain")
+    capital_loss: float = Field(alias="capital-loss")
+    hours_per_week: int = Field(alias="hours-per-week")
+    native_country: str = Field(alias="native-country")
+
+
+class MultipleDataInputs(BaseModel):
+    inputs: List[InputSchema]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "inputs": [
+                    {
+                        "age": 20,
+                        "workclass": "State-gov",
+                        "fnlgt": 77516,
+                        "education": "Bachelors",
+                        "education-num": 13,
+                        "marital-status": "Never-married",
+                        "occupation": "Adm-clerical",
+                        "relationship": "Not-in-family",
+                        "race": "White",
+                        "sex": "Male",
+                        "capital-gain": 2174,
+                        "capital-loss": 0,
+                        "hours-per-week": 40,
+                        "native-country": "United-States",
+                    }
+                ]
+            }
+        }
+
+
+# Instantiate the app.
+app = FastAPI()
+
+
+# Define a GET on the specified endpoint.
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hi, this is the Welcome message for the api!"}
+
+
+@app.post("/prediction")
+async def predict(item: MultipleDataInputs):
+    input_df = pd.DataFrame(jsonable_encoder(item.inputs))
+    data_pred, _, _, _ = process_data(
+        input_df, categorical_features=[
+            "workclass",
+            "education",
+            "marital-status",
+            "occupation",
+            "relationship",
+            "race",
+            "sex",
+            "native-country"], label=None, training=False, encoder=encoder, lb=lb)
+
+    predictions = '<=50k' if model.predict(data_pred) == 0 else '>50k'
+    return {"prediction": predictions}
