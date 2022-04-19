f512af41a9c478c3805283ae021d00f8bd176a95
João Santos <jfasantos@gmail.com>
2022-04-19 23:17:02 +0100

    Adding apis and api_testing code

--- test_api.py
+++ test_api.py
@@ -14,25 +14,47 @@ def test_read_main():
     assert response.json() == {"greeting": "Hi, this is the Welcome message for the api!"}
 
 
-def test_get_prediction():
+def test_get_prediction_negative():
     data_test = {
         "age": 20,
         "workclass": "State-gov",
         "fnlgt": 77516,
         "education": "Bachelors",
-        "education-num": 13,
+        "education-num": 0,
         "marital-status": "Never-married",
         "occupation": "Adm-clerical",
         "relationship": "Not-in-family",
         "race": "White",
         "sex": "Male",
-        "capital-gain": 2174.0,
+        "capital-gain": 0.0,
         "capital-loss": 0.0,
-        "hours-per-week": 40,
+        "hours-per-week": 40.0,
         "native-country": "United-States"
     }
     response = client.post('/predict', json=data_test)
     print(f"status_code :{response.status_code}")
-    # assert response.status_code == 200
-    # assert response.json() == {"prediction": "<50k"}
+    assert response.status_code == 200
+    assert response.json() == {"prediction": "<=50k"}
 
+
+def test_get_prediction_positive():
+    data_test = {
+        "age": 53,
+        "workclass": "Private",
+        "fnlgt": 123011,
+        "education": "Masters",
+        "education-num": 14,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0.0,
+        "capital-loss": 0.0,
+        "hours-per-week": 45.0,
+        "native-country": "United-States"
+    }
+    response = client.post('/predict', json=data_test)
+    print(f"status_code :{response.status_code}")
+    assert response.status_code == 200
+    assert response.json() == {"prediction": ">50k"}
