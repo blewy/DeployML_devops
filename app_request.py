65617d80b40f88a1209ab592e447d654015915be
João Santos <jfasantos@gmail.com>
2022-04-23 10:54:02 +0100

    Adding heroku app request and process screenshots

---
+++ app_request.py
@@ -0,0 +1,25 @@
+import requests
+
+if __name__ == "__main__":
+    data = {
+        "age": 53,
+        "workclass": "Private",
+        "fnlgt": 123011,
+        "education": "Masters",
+        "education-num": 14,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0.0,
+        "capital-loss": 0.0,
+        "hours-per-week": 45.0,
+        "native-country": "United-States"
+    }
+
+    response = requests.post(url='https://salarypredictionudacity.herokuapp.com/predict', json=data,
+                             headers={"Content-Type": "application/json; charset=utf-8"})
+    print(response.status_code)
+    print(response.reason)
+    print(response.json())
